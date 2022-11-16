37d6a13e8c7a07181c84ae21c7ee8c03d7763f4a
gnos28 <gnos28@gmail.com>
2022-11-16 12:20:22 +0100

    css improvements

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,11 @@ import styles from "./Layout.module.scss";
 type Children = { children: ReactNode };
 
 const Layout = ({ children }: Children) => {
-  return <div className={styles.layoutContainer}>{children}</div>;
+  return (
+    <div className={styles.layoutContainer}>
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
