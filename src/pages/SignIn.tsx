736fc124e91f2f7432f402bf42d738ef77a2d223
Reenauud <renaud.raymond43@hotmail.fr>
2023-01-26 15:29:25 +0100

    startmodal

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadToken from "../components/LoadToken";
+import styles from "./Signin.module.scss";
 
 const SignIn = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [modal, setModal] = useState(false);
 
   const [showLoadToken, setShowLoadToken] = useState(false);
   const navigate = useNavigate();
@@ -14,12 +16,17 @@ const SignIn = () => {
     if (token) {
       navigate("/");
     }
-  });
+  }, []);
+
+  const openModal = () => {
+    setModal(!modal);
+  };
 
   return (
     <div>
       Email{" "}
       <input
+        placeholder="email"
         onChange={(e) => {
           setUsername(e.target.value);
         }}
@@ -28,6 +35,7 @@ const SignIn = () => {
       Password{" "}
       <input
         type="password"
+        placeholder="password"
         onChange={(e) => {
           setPassword(e.target.value);
         }}
@@ -37,6 +45,14 @@ const SignIn = () => {
       {showLoadToken === true && (
         <LoadToken username={username} password={password} />
       )}
+      <button
+        onClick={() => {
+          openModal();
+        }}
+      >
+        inscription
+      </button>
+      {modal === true ? <div>aaaaa</div> : null}
     </div>
   );
 };
