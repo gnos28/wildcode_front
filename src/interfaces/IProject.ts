a0535ce6b45f43e5af73a21e1d1646f59ef0a128
gnos28 <gnos28@gmail.com>
2023-01-26 12:22:48 +0100

    newProject modal ok with some API logic

---
+++ src/interfaces/IProject.ts
@@ -0,0 +1,17 @@
+export type IProject = {
+  id: string;
+  id_storage_number: string;
+  name: string;
+  description: string;
+  isPublic: boolean;
+  nb_likes: number;
+  nb_views: number;
+  file: { language: string }[];
+};
+
+export type CreateProject = {
+  userId: number;
+  name: string;
+  description: string;
+  isPublic: boolean;
+};
