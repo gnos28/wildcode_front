f424c588dc84276a7fb9d3881931708c08788446
gnos28 <gnos28@gmail.com>
2023-03-30 14:18:18 +0200

    fix 5

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,6 +2,8 @@
 # deploy.sh
 
 docker compose -f docker-compose.deploy.yml down
+docker stop $(docker ps --filter name=front* -q)
+docker rmi -f $(docker images gnos28/wildcode-front -q)
 docker compose -f docker-compose.deploy.yml pull
 docker compose -f docker-compose.deploy.yml up >~/wildcode/front/logs/log.compose.$(date +"%s") 2>&1 &
 disown
