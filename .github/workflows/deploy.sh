1c3d7d2d30f13d6a2d73945391b26a12e6f20fa6
gnos28 <gnos28@gmail.com>
2023-03-30 12:56:34 +0200

    fix 3

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # deploy.sh
 
 docker compose -f docker-compose.deploy.yml down
 docker compose -f docker-compose.deploy.yml pull
-docker compose -f docker-compose.deploy.yml up -d
+docker compose -f docker-compose.deploy.yml up >~/wildcode/front/logs/log.compose.$(date +"%s") 2>&1 &
+disown
